08aaa62483f924a7617119d918118d79b8640d22
mariagrs <>
2021-04-09 11:00:51 +0000

    assignment

--- task.py
+++ task.py
@@ -1,26 +1,25 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python task.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+
+class Client:
+  def __init__(self, name, lastname, age,account_number,bank_name, total_amount_cash):
+    self.name = name
+    self.lastname = lastname
+    self.age = age
+    self.account_number = account_number
+    self.bank_name = bank_name
+    self.total_amount_cash = total_amount_cash
+  
+  def print_information (self):
+    print("\nName :", self.name ,"\nLastname : ", self.lastname, "\nAge : ", self.age, "\nAccount number : ", self.account_number, "\nBank name : ", self.bank_name, "\nTotal amount : ", self.total_amount_cash, "$")
+
+class Bank:
+
+  def __init__(self, name):
+    self.name = name
+
+
+
+client1 = Client('Maria','Graus','22',1234, 'SG', 3000)
+client2 = Client('Andre','Graus','21',1354, 'SG', 3500)
+
+client1.print_information()
+client2.print_information()
